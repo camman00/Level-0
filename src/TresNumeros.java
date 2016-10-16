1194045b9990036d9efe0b9d1faae98efeba8155
QUAGMIRE <league@WTS11.attlocal.net>
2016-10-16 13:05:14 -0700

    Uploaded

--- src/TresNumeros.java
+++ src/TresNumeros.java
@@ -8,15 +8,16 @@ public class TresNumeros {
 		int one = Integer.parseInt(numerouno);
 		int two = Integer.parseInt(numerodos);
 		int three = Integer.parseInt(numerotres);
-		for (int i = 0; i < args.length; i++) {
-			if (one > two && one > three) {
-				System.out.println(numerouno);
-			}
-			if (two > one && two > three) {
+		if (one > two && one > three) {
+			System.out.println(numerouno);
+			if (two > three && two > one) {
 				System.out.println(numerodos);
-			}
-			if (three > one && three > two) {
 				System.out.println(numerotres);
+			}
+			else {
+				System.out.println(numerotres);
+				System.out.println(numerodos);
+				
 			}
 		}
 		
